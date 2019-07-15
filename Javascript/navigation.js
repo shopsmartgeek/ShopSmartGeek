138d836fd055840887463305716f46bf53569818
Tyler Pearce <tpearce01@users.noreply.github.com>
2019-07-14 18:04:44 -0700

    Basic meta tags, cleanup

--- Javascript/navigation.js
+++ Javascript/navigation.js
@@ -1,19 +1,18 @@
-var currentiframe = document.getElementById("ifr2");
+//var currentiframe = document.getElementById("ifr2");
 
-function LoadHome() {
-    SwapFrame()
-    currentiframe.src = "catalog.html";
-    currentiframe.height = "1000vw";
-}
+//function LoadHome() {
+//    SwapFrame()
+//    currentiframe.src = "catalog.html";
+//    currentiframe.height = "1000vw";
+//}
 
-function SwapFrame() {
-    currentiframe.height = "0px";
-    if (currentiframe == document.getElementById("ifr1")) {
-        currentiframe = document.getElementById("ifr2");
-    } else {
-        currentiframe = document.getElementById("ifr1");
-    }
-}
+//function SwapFrame() {
+//    currentiframe.height = "0px";
+//    if (currentiframe == document.getElementById("ifr1")) {
+//        currentiframe = document.getElementById("ifr2");
+//    } else {
+//        currentiframe = document.getElementById("ifr1");
+//    }
+//}
 
-setTimeout(function () { LoadHome(); }, 100);
-write();
+//setTimeout(function () { LoadHome(); }, 100);
