6effe198a349d1fc73a59c30cb0636b9ffafb56a
Tyler Pearce <tpearce01@users.noreply.github.com>
2019-07-27 07:37:59 -0700

    Nav Menu, Cleanup, New Pages

--- Javascript/navigation.js
+++ Javascript/navigation.js
@@ -1,18 +1,8 @@
-//var currentiframe = document.getElementById("ifr2");
-
-//function LoadHome() {
-//    SwapFrame()
-//    currentiframe.src = "catalog.html";
-//    currentiframe.height = "1000vw";
-//}
-
-//function SwapFrame() {
-//    currentiframe.height = "0px";
-//    if (currentiframe == document.getElementById("ifr1")) {
-//        currentiframe = document.getElementById("ifr2");
-//    } else {
-//        currentiframe = document.getElementById("ifr1");
-//    }
-//}
-
-//setTimeout(function () { LoadHome(); }, 100);
+function updateMenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
